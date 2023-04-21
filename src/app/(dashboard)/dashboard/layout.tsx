a2de2b2755803a79b99669469ae04d7320bfe958
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-04-21 17:04:47 +0400

    change logo

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -57,7 +57,7 @@ const Layout = async ({ children }: LayoutProps) => {
       </div>
       <div className="hidden h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 md:flex">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
-          <Icons.Logo className="h-8 w-auto text-indigo-600" />
+          <Icons.Omo className="h-8 w-auto text-indigo-600" />
         </Link>
         {friends.length > 0 ? (
           <div className="text-xs font-semibold leading-6 text-gray-400">
